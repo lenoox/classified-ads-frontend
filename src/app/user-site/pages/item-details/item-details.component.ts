f55946e9fb0a5316f83ec21d375eb0a88b33ee96
lenoox <62141973+lenoox@users.noreply.github.com>
2020-04-05 23:35:03 +0200

    refactored home and details pages

--- src/app/user-site/pages/item-details/item-details.component.ts
+++ src/app/user-site/pages/item-details/item-details.component.ts
@@ -23,4 +23,10 @@ export class ItemDetailsComponent implements OnInit {
       this.ItemList = data;
     });
   }
+  calculateDiff(dateSent){
+    let currentDate = new Date();
+    dateSent = new Date(dateSent);
+
+    return Math.floor((Date.UTC(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()) - Date.UTC(dateSent.getFullYear(), dateSent.getMonth(), dateSent.getDate()) ) /(1000 * 60 * 60 * 24));
+  }
 }
